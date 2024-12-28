e553ea261409bea17bdee372485d8dfb5c0656b6
mhatresahil <mhatresahil1006@gmail.com>
2024-12-28 16:29:04 -0500

    baseline nn

--- gold.py
+++ gold.py
@@ -1,7 +1,9 @@
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 import torch
-from torch.utils.data import TensorDataset, DataLoader
+from torch.utils.data import DataLoader, TensorDataset
+import torch.nn as nn
+import matplotlib.pyplot as plt
 
 #Load the data
 df = pd.read_csv('gold_rate.csv')
@@ -22,7 +24,7 @@ def preprocess_gold(df):
     norm_cols = [f'gold_val_lag{i}' for i in  range(1,lags+1)]
     norm_cols.append('Value')
     scaler = StandardScaler()
-    df[norm_cols] = scaler.fit_transform(df[norm_cols])
+    df.loc[:,norm_cols] = scaler.fit_transform(df[norm_cols])
     
     #Splitting into training and testing data
     train_data = df.sample(frac = 0.75, random_state = 19)
@@ -44,3 +46,62 @@ def preprocess_gold(df):
     return train_dataloader, test_dataloader
 
 train_dataloader, test_dataloader = preprocess_gold(df)
+
+#Define a Baseline Neural Network model
+class goldrate_predictorNN(nn.Module):
+    def __init__(self):
+        super(goldrate_predictorNN, self).__init__()
+        self.fn1 = nn.Linear(5,10)
+        self.fn2 = nn.Linear(10,20)
+        self.fn3 = nn.Linear(20,20)
+        self.fn4 = nn.Linear(20,10)
+        self.fn5 = nn.Linear(10,5)
+        self.fn6 = nn.Linear(5,1)
+        self.relu = nn.Tanh()
+        
+    def forward(self, x):
+        x = self.fn1(x)
+        x = self.relu(x)
+        x = self.fn2(x)
+        x = self.relu(x)
+        x = self.fn3(x)
+        x = self.relu(x)
+        x = self.fn4(x)
+        x = self.relu(x)
+        x = self.fn5(x)
+        x = self.relu(x)
+        x = self.fn6(x)
+        return x
+
+model = goldrate_predictorNN()
+
+criterion = nn.MSELoss()
+optimizer = torch.optim.Adam(model.parameters(), lr = 0.001)
+
+num_epochs = 100
+train_loss = []
+for epoch in range(num_epochs):
+    for inputs, targets in train_dataloader:
+        optimizer.zero_grad()
+        outputs = model(inputs)
+        loss = criterion(outputs, targets.view(-1, 1))
+        loss.backward()
+        optimizer.step()
+    train_loss.append(loss.item())
+    print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item():.4f}')
+
+model.eval()
+with torch.no_grad():
+    test_loss = 0.0
+    for inputs, targets in test_dataloader:
+        outputs = model(inputs)
+        test_loss += criterion(outputs, targets.view(-1, 1)).item()
+
+    avg_test_loss = test_loss / len(test_dataloader)
+    print(f'Average Test Loss: {avg_test_loss:.4f}')
+    
+epochs = (range(1, num_epochs+1))
+plt.plot(epochs, train_loss,'b',label='Training Loss')
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
+plt.show()
